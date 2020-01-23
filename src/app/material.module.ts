e3e1dbeea84042d054e5d61ec75455c3d6265bca
ydia <yannickdia31@gmail.com>
2020-01-23 10:04:54 +0100

    modif materialModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 
 
@@ -12,12 +18,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule,
     MatMenuModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatGridListModule
   ],
   exports: [
     MatMenuModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatGridListModule
+
   ]
 })
 export class MaterialModule { }
